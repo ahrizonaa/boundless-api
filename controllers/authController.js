c6a4a6b977401e46fdf11a02127e3fd37a4ba026
missahrizona <princessssahri@gmail.com>
2022-05-17 08:28:01 -0400

    executed by .sh script

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,39 @@
-import { Controller } from './controller.js';
+import { Controller } from "./controller.js";
 
 class AuthController extends Controller {
-	constructor(c, d) {
-		super(c, d);
-	}
+  constructor(c, d) {
+    super(c, d);
+
+    this.post("/exists", async (req, res) => {
+      let search = await this.db
+        .collection("Users")
+        .findOne({ tel: req.body.tel });
+      res.send(search);
+    });
+
+    this.post("/requestcode", async (req, res) => {
+      let code = Math.floor(Math.random() * 999999);
+      await this.twilio.messages
+        .create({
+          body: code,
+          to: req.body.tel,
+          from: process.env.TWILIO_PHONE_NUMBER,
+        })
+        .then((msg) => {
+          msg.code = code;
+          res.send(msg);
+        });
+    });
+
+    this.post("/createaccount", async (req, res) => {
+      let result = await this.db.collection("Users").insertOne({
+        tel: req.body.tel,
+        displayname: req.body.displayname,
+      });
+
+      res.send(result);
+    });
+  }
 }
 
 export { AuthController };
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,19 +1,22 @@
-import { Router } from 'express';
-import { Utils } from '../lib/utils.js';
-import { config } from 'dotenv';
+import { Router } from "express";
+import { Utils } from "../lib/utils.js";
+import { config } from "dotenv";
+import { twilioclient } from "../lib/twilioclient.js";
 
 class Controller extends Router {
-	constructor(client, db) {
-		super();
-		config();
-		this.utils = Utils;
-		this.client = client;
-		this.db = db;
-	}
+  constructor(client, db) {
+    super();
+    config();
+    this.utils = Utils;
+    this.client = client;
+    this.db = db;
+    this.twilio = twilioclient;
+  }
 
-	db;
-	client;
-	utils;
+  db;
+  client;
+  utils;
+  twilio;
 }
 
 export { Controller };
