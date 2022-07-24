996f9647cdeb4d64eb7cbbb930b6ff2c21831e85
Pretty Lisa <missahrizona@outlook.com>
2022-07-24 03:56:46 -0500

    new device

--- controllers/authController.js
+++ controllers/authController.js
@@ -31,9 +31,10 @@ class AuthController extends Controller {
 
     this.post("/requestcode", async (req, res) => {
       let code = Math.floor(Math.random() * (999999 - 100000 + 1) + 100000);
+      let msg = `One time code:\n${code}`;
       await this.twilio.messages
         .create({
-          body: code,
+          body: msg,
           to: req.body.tel,
           from: process.env.TWILIO_PHONE_NUMBER,
         })
