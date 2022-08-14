44c6136f4ee44c691a58cca9a32aa6e4940425df
Miss Ahrizona <missahrizona@outlook.com>
2022-08-14 04:12:25 -0500

    edited home

--- api/home.ts
+++
@@ -1,5 +0,0 @@
-import type { VercelRequest, VercelResponse } from  '@vercel/node';
-
-export function (req: VercelRequest, res: VercelResponse) {
-  res.send('Mydea Serverless API: ' + req.query.name);
-};
