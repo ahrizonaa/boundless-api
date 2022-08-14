7de8397b212d210c7d0eb74962b1d48a162096cb
Miss Ahrizona <missahrizona@outlook.com>
2022-08-14 04:03:45 -0500

    changed serverless func name

--- api/home.ts
+++ api/home.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from  '@vercel/node';
 
 export function (req: VercelRequest, res: VercelResponse) {
-  res.send('Mydea Serverless API');
+  res.send('Mydea Serverless API: ' + req.query.name);
 };
