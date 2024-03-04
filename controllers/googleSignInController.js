794742699b5b9082b083c5285d0166d8a1029286
ahrizona <missahrizona@outlook.com>
2024-03-03 22:59:14 -0600

    finish migrate to central api

--- controllers/googleSignInController.js
+++ controllers/googleSignInController.js
@@ -1,5 +1,5 @@
 import { Controller } from './controller.js';
-import { VerifyGoogleUser } from '../lib/lib-google.js';
+import { VerifyGoogleUser, SyncUserWithDB } from '../lib/lib-google.js';
 
 class GoogleSignInController extends Controller {
 	constructor(c, d) {
@@ -13,7 +13,7 @@ class GoogleSignInController extends Controller {
 				return;
 			}
 
-			let response = await this.SyncUserWithDB(googleprofile);
+			let response = await SyncUserWithDB(googleprofile);
 			response.code = req.body.code;
 
 			res.send(response);
@@ -22,52 +22,6 @@ class GoogleSignInController extends Controller {
 		this.get('/appsettings', async (req, res) => {
 			res.send({ client_id: process.env.client_id });
 		});
-	}
-
-	async SyncUserWithDB(googleprofile) {
-		let user = await this.db.collection('Users').findOne({
-			'googleprofile.email': googleprofile.email
-		});
-
-		if (user == null) {
-			let newuser = {
-				googleprofile: googleprofile
-			};
-
-			let insert = await this.db.collection('Users').insertOne(newuser);
-
-			if (insert.acknowledged) {
-				return {
-					...newuser,
-					_id: insert.insertedId,
-					isInsert: true,
-					isAuthenticated: true
-				};
-			} else
-				return {
-					error: 'Unable to create new user using Google Sign In'
-				};
-		} else if (user.googleprofile.exp < googleprofile.exp) {
-			let refresh = await this.db.collection('Users').updateOne(
-				{
-					'googleprofile.email': googleprofile.email
-				},
-				{ $set: { googleprofile: googleprofile } }
-			);
-
-			if (refresh.acknowledged) {
-				user.isRefresh = true;
-				user.isAuthenticated = true;
-				user.googleprofile = googleprofile;
-				return user;
-			}
-		} else {
-			return {
-				...user,
-				isRefresh: false,
-				isAuthenticated: true
-			};
-		}
 	}
 }
 
