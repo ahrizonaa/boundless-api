c8612fa87f9c67acb9f9d8126bac20d44d4f599a
Mark <markus.anthony.garcia@gmail.com>
2024-06-13 03:26:09 -0500

    test

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,6 +4,12 @@ class IndexController extends Controller {
 	constructor(c, d) {
 		super(c, d);
 		this.get('/', (req, res) => {
+			let host = req.get('host');
+			let origin = req.get('origin');
+			console.log({
+				host,
+				origin
+			});
 			res.sendFile(process.cwd() + '/public/index.html');
 		});
 	}
