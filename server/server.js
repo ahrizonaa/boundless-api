ced445c379d1811e4faaf14db2c9726d1ae6be9f
Mark <markus.anthony.garcia@gmail.com>
2024-06-13 04:14:28 -0500

    test

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ server.use(cors());
 server.use(express.json({ limit: '50mb' }));
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.static('public'));
-server.use('/', defaultController);
+// server.use('/', defaultController);
 server.use((req, res, next) => {
 	const host = req.get('host');
 
