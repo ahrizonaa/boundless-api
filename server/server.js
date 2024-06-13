27c294562ccc4f9d16157e49a5197ec7c8cf7313
Mark <markus.anthony.garcia@gmail.com>
2024-06-13 03:56:41 -0500

    test

--- server/server.js
+++ server/server.js
@@ -1,15 +1,7 @@
-const server = express();
-
-import { WebSocketServer } from 'ws';
-
-const wss = new WebSocketServer({
-	noServer: true
-});
-
 import express from 'express';
+import { WebSocketServer } from 'ws';
 import cors from 'cors';
 import { mongoClient } from '../lib/mongoClient.js';
-
 import {
 	IndexController,
 	NimblewearController,
@@ -18,16 +10,38 @@ import {
 	CoreController
 } from '../controllers/index.js';
 
+const server = express();
+
+const wss = new WebSocketServer({
+	noServer: true
+});
+
+const defaultController = new IndexController(mongoClient, 'Mydeas');
+
+const hostControllerMap = {
+	'boundless-api-ltlq6.ondigitalocean.app': defaultController,
+	'api.nimbelwear.app': new NimblewearController(mongoClient, 'NimbelWear'),
+	'api.jolloftkn.com': new JollofController(mongoClient, 'Jollof'),
+	'api.nightowlvibes.app': null
+};
+
 server.set('json spaces', 4);
 server.use(cors());
 server.use(express.json({ limit: '50mb' }));
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.static('public'));
-server.use('/', new IndexController(mongoClient, 'Mydeas'));
-server.use('/nimbelwear', new NimblewearController(mongoClient, 'NimbelWear'));
-server.use('/google', new GoogleSignInController(mongoClient, 'Storage'));
-server.use('/jollof', new JollofController(mongoClient, 'Jollof'));
-server.use('/core', new CoreController(mongoClient, 'Storage'));
+server.use('/', defaultController);
+server.use((req, res, next) => {
+	const host = req.get('host');
+
+	const controllerRouter = hostControllerMap[host] || defaultController;
+
+	controllerRouter(req, res, next);
+});
+// server.use('/nimbelwear');
+// server.use('/google', new GoogleSignInController(mongoClient, 'Storage'));
+// server.use('/jollof', new JollofController(mongoClient, 'Jollof'));
+// server.use('/core', new CoreController(mongoClient, 'Storage'));
 
 wss.on('connection', async function (ws) {
 	console.log('new wss conneciton established');
